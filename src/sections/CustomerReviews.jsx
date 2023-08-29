dd1cd1d01e324167531c7cde5008c6cb0648fa62
Noud63 <noudvandun@gmail.com>
2023-08-29 12:46:45 +0200

    first commit

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CustomerReviews = () => {
+  return (
+    <section>
+       
+    </section>
+  )
+}
+
+export default CustomerReviews
+
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+
+
+export {
+    Hero,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    Footer,
+    PopularProducts,
+    SuperQuality,
+    CustomerReviews
+}
